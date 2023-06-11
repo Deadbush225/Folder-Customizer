cbd11901886d94a9fd78043c10dda54d8d9ec1e4
Eliazar Inso <eliazar225ninso@gmail.com>
2023-06-11 23:07:29 +0800

    removed console window

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,29 @@
 #include <iostream>
 
 #include <QtWidgets/QApplication>
+// #include <QtWidgets/QWidget>
+
+// #include <QtCore/QDebug>
+
+#include <stdio.h>
+#include <windows.h>
 
 #include "Window/window.h"
 
 int main(int argc, char* argv[]) {
+#if defined(Q_OS_WIN)
+    ::ShowWindow(::GetConsoleWindow(), SW_HIDE);  // hide console window
+#endif
+
     auto app = new QApplication(argc, argv);
 
     FolderCustomizerWindow window = FolderCustomizerWindow();
     window.show();
+
+    // QWidget window = QWidget();
+    // window.show();
+
+    // qDebug() << "test";
 
     app->exec();
     delete app;
